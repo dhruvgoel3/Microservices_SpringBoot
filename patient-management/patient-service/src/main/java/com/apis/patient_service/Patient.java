853932d1b25027880830a35932733abeb1ac86d1
dhruvgoel3 <dhruvgoel1100@gmail.com>
2026-05-23 00:01:36 +0530

    Patient Management Microservices Project

---
+++ patient-management/patient-service/src/main/java/com/apis/patient_service/Patient.java
@@ -0,0 +1,36 @@
+package com.apis.patient_service;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @Email
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+
+
+}
---
+++ patient-management/patient-service/src/main/java/com/apis/patient_service/repository/PatientRepository.java
@@ -0,0 +1,4 @@
+//package com.apis.patient_service.repository;
+//
+//public interface PatientRepository extends  {
+//}
